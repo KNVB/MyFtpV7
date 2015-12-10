94e26c471d41b23a8ee3689d79f5a9402d2b2437
SITO3 <SITO3@WIN8-SITO3>
2015-12-10 11:19:05 +0800

    2015-12-10 11:18

---
+++ src/com/myftpserver/impl/old/DbOp.java
@@ -0,0 +1,163 @@
+package com.myftpserver.impl.old;
+
+import java.sql.*;
+
+import com.myftpserver.*;
+import com.myftpserver.exception.*;
+import com.myftpserver.handler.FtpSessionHandler;
+import com.myftpserver.interfaces.FileManager;
+import com.myftpserver.interfaces.UserManager;
+
+import org.apache.log4j.Logger;
+
+
+public class DbOp 
+{
+	private String jdbcDriver = new String();
+	private String jdbcURL = new String();
+	private Connection dbConn = null;
+	private Logger logger = null;
+	private Configuration config = null;
+
+	public DbOp(Configuration c) throws Exception 
+	{
+		jdbcDriver = "org.sqlite.JDBC";
+		jdbcURL = "jdbc:sqlite:user.db";
+		logger=c.getLogger();
+		config=c;
+		Class.forName(jdbcDriver);
+		dbConn = DriverManager.getConnection(jdbcURL);
+	}
+	public User login(FtpSessionHandler fs, String password) throws LoginFailureException
+	{
+		User u=null;
+		int result=0;
+		String sql;
+		String userName=fs.getUserName();
+		ResultSet rs = null;
+		PreparedStatement stmt = null;
+		try
+		{
+			sql="select * from user where user_name=? and password=? and active=1";
+			stmt=dbConn.prepareStatement(sql);
+			stmt.setString(1, userName);
+			stmt.setString(2, password);
+			rs=stmt.executeQuery();
+			if (rs.next())
+			{
+			  u=new User();
+			  //u.setHomeDir(rs.getString("home_dir"));
+			  u.setName(rs.getString("user_name"));
+			  u.setPassword(rs.getString("password"));
+			  u.setQuota(rs.getInt("quota"));
+			  u.setActive(true);
+			}
+			else
+				result=UserManager.INVAILD_USERNAME_OR_PASSWORD;
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		} 
+		finally 
+		{
+			releaseResource(rs, stmt);
+		}
+		if (result!=0)
+		{
+			u=null;
+			throw new LoginFailureException(config.getFtpMessage("530_Invalid_Login")); 
+		}
+		return u;
+	}
+	public void loadACL(User user)
+	{
+		String sql;
+		ResultSet rs = null;
+		PreparedStatement stmt = null;
+		try
+		{
+			sql ="select vir_dir,permission,phy_dir ";
+			sql+="from virdir_2_phydir where user_name=? and active=1";
+			stmt=dbConn.prepareStatement(sql);
+			stmt.setString(1, user.getName());
+			rs=stmt.executeQuery();
+			while (rs.next())
+			{
+				logger.debug("vir_dir="+rs.getString("vir_dir")+",phy_dir="+rs.getString("phy_dir")+",permission="+rs.getString("permission"));
+				if ((rs.getString("vir_dir")!=null) && (!rs.getString("vir_dir").equals("")))
+				{
+					if (rs.getString("phy_dir")==null)
+						user.addClientPathACL(rs.getString("vir_dir").trim(), rs.getString("permission").trim() +"\tnull");
+					else	
+						user.addClientPathACL(rs.getString("vir_dir").trim(), rs.getString("permission").trim() +"\t"+rs.getString("phy_dir").trim());
+				}
+				if ((rs.getString("phy_dir")!=null) && (!rs.getString("phy_dir").equals("")))
+				{
+					if (rs.getString("vir_dir")==null)
+						user.addServerPathACL(rs.getString("phy_dir").trim(), rs.getString("permission").trim()+"\tnull");
+					else	
+						user.addServerPathACL(rs.getString("phy_dir").trim(), rs.getString("permission").trim()+"\t"+rs.getString("vir_dir").trim());
+					
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			releaseResource(rs, stmt);
+		}
+	}
+	public String getRealPath(FtpSessionHandler fs,String virPath,String permission)throws AccessDeniedException,PathNotFoundException 
+	{
+		User user=fs.getUser();
+		String realPath=null,pathPerm=null;
+		String clientPath=Utility.resolveClientPath(logger,fs.getCurrentPath(), virPath);
+
+		logger.debug("user ="+user.getName()+",currentPath="+fs.getCurrentPath()+",virPath="+virPath+",permission="+permission+",clientPath="+clientPath);
+		realPath=Utility.getRealPath(fs,clientPath, permission);
+		logger.debug("user ="+user.getName()+",currentPath="+fs.getCurrentPath()+",virPath="+virPath+",permission="+permission+",clientPath="+clientPath+",realPath="+realPath+",pathPerm="+pathPerm);
+		return realPath;
+	}
+	public void getRealHomePath(FtpSessionHandler fs)throws AccessDeniedException,PathNotFoundException 
+	{
+		fs.setCurrentPath("/");
+		getRealPath(fs,"/",FileManager.READ_PERMISSION);
+	}
+	public void close() throws Exception 
+	{
+		dbConn.close();
+		dbConn = null;
+	}
+	
+	private void releaseResource(ResultSet r, PreparedStatement s) 
+	{
+		if (r != null) 
+		{
+			try 
+			{
+				r.close();
+			} 
+			catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		if (s != null) 
+		{
+			try 
+			{
+				s.close();
+			} 
+			catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		r = null;
+		s = null;
+	}
+}
---
+++ src/com/myftpserver/impl/old/MyUserManager.java
@@ -0,0 +1,114 @@
+package com.myftpserver.impl.old;
+
+import java.util.Vector;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import com.myftpserver.User;
+import com.myftpserver.impl.DbOp;
+import com.myftpserver.UserGroup;
+import com.myftpserver.exception.*;
+import com.myftpserver.Configuration;
+import com.myftpserver.interfaces.UserManager;
+import com.myftpserver.handler.FtpSessionHandler;
+
+public class MyUserManager extends UserManager  
+{
+	String strSql=new String();
+	ArrayList<Object> values=null;
+	DbOp dbo=null;
+	ResultSet rs=null;
+	public MyUserManager(Configuration c) 
+	{
+		super(c);
+		try 
+		{
+			dbo=new DbOp(c);
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+			dbo=null;
+		}
+	}
+	@Override
+	public Vector<User> listAllUser() 
+	{
+		return null;
+	}
+	@Override
+	public Vector<UserGroup> listAllUserGroup() 
+	{
+		return null;
+	}
+	@Override
+	public int addUser(User u) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int deleteUser(String uN) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int setPassword(User u) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int addUserGroup(UserGroup userGroup) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int deleteUserGroup(UserGroup userGroup) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int addUserToUserGroup(User user, UserGroup userGroup) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int removeUserFromUserGroup(User user, UserGroup userGroup) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public User login(FtpSessionHandler fs, String password)throws LoginFailureException, AccessDeniedException,InvalidHomeDirectoryException 
+	{
+		User u=dbo.login(fs, password);
+		try
+		{
+			dbo.loadACL(u);
+			fs.setUser(u);
+			dbo.getRealHomePath(fs);
+			logger.debug("Client path ACL size="+u.getClientPathACL().size());
+			logger.debug("Server path ACL size="+u.getServerPathACL().size());
+		}
+		catch (PathNotFoundException er)
+		{
+			throw new InvalidHomeDirectoryException(config.getFtpMessage("530_Home_Dir_Not_Found"));
+		}
+		return u;		
+	}
+	@Override
+	public void close() 
+	{
+		if (dbo!=null)
+		{
+			try 
+			{
+				dbo.close();
+			} 
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		dbo=null;		
+	}
+
+}
