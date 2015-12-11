38a35b746d9313da4bdc04be84a88d20d792d428
cstsang <cstsang@notebook>
2015-12-11 22:52:33 +0800

    2015-12-11 22:52

--- src/com/myftpserver/PassiveServer.java
+++ src/com/myftpserver/PassiveServer.java
@@ -15,13 +15,6 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
-
-
-
-
-
-
-
 import com.util.Utility;
 import com.myftpserver.handler.FtpSessionHandler;
 import com.myftpserver.handler.ReceiveFileHandler;
