2d3837f024bb047c385486350a5d41112a22a3fe
SITO3 <cstsang@hko.gov.hk>
2015-12-30 17:59:21 +0800

    2015-12-30 17:59

--- src/com/myftpserver/MyFtpServer.java
+++ src/com/myftpserver/MyFtpServer.java
@@ -1,7 +1,11 @@
 package com.myftpserver;
 
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 import java.io.File;
 import java.util.Stack;
@@ -10,6 +14,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.Configurator;
+
+import com.myftpserver.channelinitializer.CommandChannelInitializer;
 /*
  * Copyright 2004-2005 the original author or authors.
  *
@@ -68,13 +74,75 @@ public final class MyFtpServer
 		connectionCount--;
 		logger.info("Concurrent Connection Count:"+connectionCount);
 	}	
+	/**
+	 * Check whether the concurrent connection is over the limit
+	 * @return true when the concurrent connection is over the limit
+	 */
+	public synchronized boolean isOverConnectionLimit()
+	{
+		if (connectionCount<serverConfig.getMaxConnection())
+		{	
+			connectionCount++;
+			return false;
+		}
+		else			
+			return true;
+	}
+//-------------------------------------------------------------------------------------------	
+	/**
+	 * Get next available passive port for data transfer
+	 * @return port no.<br>
+	 *         -1 when no. passive port is available.
+	 */
+	public synchronized int getNextPassivePort()
+	{
+		int nextPassivePort=-1;
+		if (serverConfig.isSupportPassiveMode())
+		{
+			if (serverConfig.isPassivePortSpecified())
+			{
+				if (passivePorts.size()>0)
+					nextPassivePort=passivePorts.pop();
+			}
+		}
+		return nextPassivePort;
+	}
+//-------------------------------------------------------------------------------------------	
+	/**
+	 * Return port no. to passive port pool
+	 * @param port the return passive port 
+	 */
+	public void returnPassivePort(int port) 
+	{
+		if (!passivePorts.contains(port))
+		{	
+			passivePorts.push(port);
+			logger.debug("Passive Port:"+port+" return");
+		}
+	}
 //-------------------------------------------------------------------------------------------	
 	/**
 	 *  start FTP server
 	 */
 	public void start()
 	{
-		
+		try 
+        {
+			ServerBootstrap bootStrap = new ServerBootstrap();
+            bootStrap.group(bossGroup, workerGroup);
+            bootStrap.localAddress(serverConfig.getServerPort());
+            bootStrap.channel(NioServerSocketChannel.class);
+            bootStrap.childHandler(new CommandChannelInitializer(this,logger));
+            bootStrap.childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+            logger.info("My FTP Server is started.");
+            // Wait until the server socket is closed.
+            bootStrap.bind();
+        } 
+        catch (Exception e) 
+        {
+			e.printStackTrace();
+			this.stop();
+		}	        
 	}
 //-------------------------------------------------------------------------------------------	
 	/**
