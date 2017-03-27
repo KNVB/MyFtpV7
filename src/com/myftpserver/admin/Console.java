83aa676e190854cf3cabf974da7f132a08c995f7
cstsang <cstsang@notebook>
2017-03-27 23:45:20 +0800

    2017-23:45

--- src/com/myftpserver/admin/Console.java
+++ src/com/myftpserver/admin/Console.java
@@ -1,98 +1,55 @@
 package com.myftpserver.admin;
 
-import java.io.File;
-import java.util.Stack;
+import javax.swing.*;
 
-import javax.swing.JMenu;
-import javax.swing.JFrame;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JToolBar;
+import java.awt.GridLayout;
 
-import java.awt.BorderLayout;
-import java.awt.event.KeyEvent;
-
-import javax.swing.JOptionPane;
-import javax.swing.WindowConstants;
-import javax.swing.border.EtchedBorder;
-
-import com.util.ConfigurationFactory;
-import com.myftpserver.abstracts.FtpServerConfig;
-import com.myftpserver.admin.popup_windows.ConnectAdminServerPopup;
-
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.Configurator;
 public class Console 
-{
-	private JMenu menu;
-	private JFrame frame;
-	private JMenuBar menuBar;
-	private JToolBar toolBar;
-	private JMenuItem menuItem;
-	
-	private static Logger logger=null;
-	private FtpServerConfig serverConfig=null;
-	public int loadConfigResult=FtpServerConfig.LOAD_FAIL;
-	public Console() throws Exception
+{	
+	public Console()
 	{
-		logger = LogManager.getLogger(Console.class.getName()); 
-		logger.debug("Log4j2 is ready.");
-		
-		ConfigurationFactory cf=new ConfigurationFactory(logger);
-		serverConfig=cf.getServerConfiguration();
-		loadConfigResult=serverConfig.load();
-	}
-	public void initUI()
-	{
-		// Create a toolbar and give it an etched border.
-		toolBar = new JToolBar();
-		toolBar.setBorder(new EtchedBorder());
-
-		frame = new JFrame(serverConfig.getConsoleHeading());
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-		frame.getContentPane().add(toolBar, BorderLayout.NORTH);
-		frame.pack();
-		
-		// Create the menu bar.
-		menuBar = new JMenuBar();
-		
-		// Build the first menu.
-		menu = new JMenu("Server");
-		menu.setMnemonic(KeyEvent.VK_S);
-		menu.getAccessibleContext().setAccessibleDescription("Server related function.");
-		// a group of JMenuItems
-		menuItem = new JMenuItem("Connect", KeyEvent.VK_C);
-		menuItem.getAccessibleContext().setAccessibleDescription("Connect to Admin. server");
-		menuItem.addActionListener(new ConnectAdminServerPopup());
-		menu.add(menuItem);
-		menuBar.add(menu);
-		frame.setJMenuBar(menuBar);
-		frame.setVisible(true);
-	}
-	public void loadConfigfailure()
-	{
-		frame = new JFrame("MyFtpServer console");
-		JOptionPane.showMessageDialog(frame,
-			    "Loading Configuration File failure.");
-	}
-	public static void main(String[] args) 
-	{
-		try
-		{
-			Console c=new Console();
-			if (c.loadConfigResult==FtpServerConfig.LOAD_FAIL)
-				c.loadConfigfailure();
-			else
-				c.initUI();
-		}
-		catch (Exception e)
-		{
+        JTextField adminServerIpAddress = new JTextField();
+        JTextField adminServerPort = new JTextField();
+        JPanel panel = new JPanel(new GridLayout(2, 2));
+       
+        panel.add(new JLabel("Admin. Server Host name/IP Address:"));
+        panel.add(adminServerIpAddress);
+        panel.add(new JLabel("Admin. Server Port No.:"));
+        panel.add(adminServerPort);
+        UIManager.put("OptionPane.cancelButtonText", "Cancel");
+        UIManager.put("OptionPane.okButtonText", "Connect");
+        int result = JOptionPane.showConfirmDialog(null, panel, "Connect to Admin. server",
+            JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        if (result == JOptionPane.OK_OPTION) {
+            System.out.println(
+                 " " + adminServerIpAddress.getText()
+                + " " + adminServerPort.getText());
+        } else {
+            System.out.println("Cancelled");
+        }
+        panel=null;
+        try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
+	public static void main(String[] args) 
+	{
+		java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(
+                                 // "javax.swing.plaf.metal.MetalLookAndFeel");
+                                 // "com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+                                //UIManager.getCrossPlatformLookAndFeelClassName());
+                            UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                new Console();
+            }
+        });		
+	}
 }
