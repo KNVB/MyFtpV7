88512b49d7c1c1a3f7861c52534d0c08e1eeaad4
cstsang <cstsang@notebook>
2017-04-04 23:21:01 +0800

    2017-04-04 23:20

--- src/com/myftpserver/admin/LoginBox.java
+++ src/com/myftpserver/admin/LoginBox.java
@@ -5,16 +5,18 @@ import javax.swing.border.Border;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginBox 
+public class LoginBox implements ActionListener
 {	
+    JFrame frame=new JFrame("Connect to Admin. server");
+    JButton okButton=new JButton("Ok");
+    JButton cancelButton=new JButton("Cancel");
 	public LoginBox()
 	{
-        JFrame frame=new JFrame("Connect to Admin. server");
 		JTextField adminServerIpAddress = new JTextField();
         JTextField adminServerPort = new JTextField();
-        JButton OkButton=new JButton("Ok");
-        JButton cancelButton=new JButton("Cancel");
         JPanel panel = new JPanel(new GridLayout(3, 2));
         Border padding = BorderFactory.createEmptyBorder(10,10,10,10);
         panel.setBorder(padding);
@@ -22,14 +24,28 @@ public class LoginBox
         panel.add(adminServerIpAddress);
         panel.add(new JLabel("Admin. Server Port No.:"));
         panel.add(adminServerPort);
-        panel.add(OkButton);
+        panel.add(okButton);
         panel.add(cancelButton);
+        okButton.addActionListener(this);
+        cancelButton.addActionListener(this);
         frame.setContentPane(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(okButton))
+		{
+			JOptionPane.showMessageDialog(frame, "Eggs are not supposed to be green.");
+		}
+		else
+		{
+			frame.dispose();
+		}
+	}
+
 	public static void main(String[] args) 
 	{
 		java.awt.EventQueue.invokeLater(new Runnable() {
