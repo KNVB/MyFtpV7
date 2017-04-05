d281c3ecb22493bdb829db4332a41dea2f969282
cstsang <cstsang@notebook>
2017-04-05 21:52:17 +0800

    The connect admin. server box changed to JDialog based and modal.

--- src/com/myftpserver/admin/dialogBox/ConnectAdminServerDialogBox.java
+++ src/com/myftpserver/admin/dialogBox/ConnectAdminServerDialogBox.java
@@ -1,4 +1,5 @@
 package com.myftpserver.admin.dialogBox;
+
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,35 +7,43 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 public class ConnectAdminServerDialogBox implements ActionListener
-{	
-    JFrame frame=new JFrame("Connect to Admin. server");
-    JButton okButton=new JButton("Ok");
+{
+	JFrame father=null; 
+	JButton okButton=new JButton("Ok");
     JButton cancelButton=new JButton("Cancel");
 	JTextField adminServerName = new JTextField();
     JTextField adminServerPort = new JTextField();
-	public ConnectAdminServerDialogBox()
-	{
-        JPanel panel = new JPanel(new GridLayout(3, 2));
-        Border padding = BorderFactory.createEmptyBorder(10,10,10,10);
-        panel.setBorder(padding);
-        panel.add(new JLabel("Admin. Server Host name/IP Address:"));
-        panel.add(adminServerName);
-        panel.add(new JLabel("Admin. Server Port No.:"));
-        panel.add(adminServerPort);
-        panel.add(okButton);
-        panel.add(cancelButton);
-        okButton.addActionListener(this);
-        cancelButton.addActionListener(this);
-        frame.setContentPane(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setLocationRelativeTo(null); // show in the center of screen
-        frame.setVisible(true);
-	}
+    JDialog dialog=new JDialog(father,"Connect to Admin. server",true);
+    public ConnectAdminServerDialogBox()
+    {
+    	 JPanel panel = new JPanel(new GridLayout(3, 2));
+         Border padding = BorderFactory.createEmptyBorder(10,10,10,10);
+         panel.setBorder(padding);
+         panel.add(new JLabel("Admin. Server Host name/IP Address:"));
+         panel.add(adminServerName);
+         panel.add(new JLabel("Admin. Server Port No.:"));
+         panel.add(adminServerPort);
+         panel.add(okButton);
+         panel.add(cancelButton);
+         okButton.addActionListener(this);
+         cancelButton.addActionListener(this);
+         dialog.setContentPane(panel);
+         dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+         dialog.pack();
+         dialog.setLocationRelativeTo(null); // show in the center of screen
+         dialog.setVisible(true);
+    }
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource().equals(okButton))
@@ -47,12 +56,14 @@ public class ConnectAdminServerDialogBox implements ActionListener
 			}
 			catch (IOException |NumberFormatException ce)
 			{
-				JOptionPane.showMessageDialog(frame, "Connection to Admin. Server failured.");
+				JOptionPane.showMessageDialog(dialog, "Connection to Admin. Server failured.");
 			}
 		}
 		else
 		{
-			frame.dispose();
+			dialog.dispose();
 		}
 	}	
+
 }
+
